6921db1b0e4e277b92436bb0b422c228c476dd05
NathanVanS <108300943+NathanVanS@users.noreply.github.com>
2022-07-04 21:20:58 -0500

    Add files via upload

---
+++ config.py
@@ -0,0 +1,23 @@
+# Dictionary file location, change if needed.
+dictionaryloc = 'data_txt_files/dictionary.txt'
+
+# Turn text file location, change if needed.
+turntextloc = 'data_txt_files/turntext.txt'
+
+# Wheel contents for file location, change if needed.
+wheeltextloc = 'data_txt_files/wheeldata.txt'
+
+# End of Round Status status file, change if needed.
+roundstatusloc = 'data_txt_files/roundstatus.txt'
+
+# number of rounds in a game
+maxrounds = 3
+
+# Vowel cost
+vowelcost = 250
+
+# final prize, you fill this in
+finalprize = "I dunno whatever Jeff Bezos is worth or something."
+
+# final round, change if needed.
+finalRoundTextLoc = 'data_txt_files/finalround.txt'
